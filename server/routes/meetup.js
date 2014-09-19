14189e55ed1993f7556c0aa737bd9379caf4d031
Caleb Brewer <calebthebrewer@gmail.com>
2014-09-18 20:28:46 -0500

    removing caching of meetup data since it is now location dependent

--- server/routes/meetup.js
+++ server/routes/meetup.js
@@ -3,25 +3,17 @@ var utils = require('./_utils'),
 
 var meetup = new Meetup('223271720383c2625e3d52287b2d7b');
 
-var categoriesCache = false,
-	meetupCache = false,
-	cacheTime = false;
+var categoriesCache = false;
 
 module.exports = function(app) {
 	app.get(utils.base + 'meetup/events', function(req, res) {
-		if (meetupCache && cacheTime > (new Date).getTime() - (1000 * 60 * 60)) {
-			res.send(meetupCache[Math.floor((Math.random() * meetupCache.length))]);
-		} else {
-			meetup.getOpenEvents({
-				lat: req.query.lat,
-				lon: req.query.lon,
-				time: '0,1d'
-			}, function(err, events) {
-				meetupCache = events.results;
-				cacheTime = (new Date).getTime();
-				res.send(events.results[Math.floor((Math.random() * events.results.length))]);
-			});
-		}
+		meetup.getOpenEvents({
+			lat: req.query.lat,
+			lon: req.query.lon,
+			time: '0,1d'
+		}, function(err, events) {
+			res.send(events.results[Math.floor((Math.random() * events.results.length))]);
+		});
 
 	});
 
